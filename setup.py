1782831ae96d681a720b81714c3f6984859002d6
Stephan Hahn <stephan@augmented-anatomy.com>
2024-06-18 10:56:44 +0200

    added cu and h in package

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 from version import VERSION
 from setuptools import setup
+from setuptools.command.build_py import build_py as BuildPy
+
+class build_py(BuildPy):
+    def run(self):
+        self.include_package_data = True
+        BuildPy.run(self)
 setup(
     name='eg3d',
     version=VERSION,
-    zip_safe=False
+    zip_safe=False,
+    include_package_data=True,
+    cmdclass={},
 )
